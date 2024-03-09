125c09810ee4efb4abf2a5e33caf311fc4778b07
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-09 10:24:18 -0300

    Create entrada.py

---
+++ dia1/entrada.py
@@ -0,0 +1,19 @@
+# Programa Exemplo
+
+# Início
+
+# Variáveis
+N1 = int(input("N1="))
+N2 = int(input("N2="))
+print("Soma = ", N1 + N2)
+# Leia num
+num = input("Digite um número: ")
+num = int(num)
+
+# dobro = num * 2
+cubo = num ** 3
+
+# Escreva "Dobro = ", dobro
+print("Seu número elevado ao cubo é: ", cubo)
+
+# Fim
