8b5dd25c6e04daf3094dfa63726a7589a21893bf
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-08 22:29:50 -0300

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,4 @@
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python:
+print(10 + 20 * 30)
+print((4 ** 2) / 30)
+print(((9 ** 4) + 2) * 6 - 1)
