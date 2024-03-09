6b153800626e5875de14b4537523a49042c65eb5
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-08 22:28:15 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,1 +1,17 @@
-
+# Esse programa calcula a média
+# shift + alt = selecionar várias linhas
+# """ Faz comentários em mais de uma linha
+"""
+documentacao
+documentacao
+documentacao
+documentacao
+documentacao
+documentacao
+"""
+print("Hello World!")
+nota1 = 9
+nota2 = 5
+nota3 = 10
+media = (nota1 + nota2 + nota3) / 3
+print(media)
