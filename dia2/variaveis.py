3bf40594f86f34c022d0db3e04edc463079de2ff
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-15 21:31:04 -0300

    Create variaveis.py

---
+++ dia2/variaveis.py
@@ -0,0 +1,21 @@
+a = 10
+a, b = 3*a, a
+print(a, b)
+
+a1 = 10
+a1 = 3*a1
+b1 = a1
+print(a1, b1)
+
+a2 = 10
+b2 = 20
+a2, b2 = 3*a2, a2
+
+print(a2, b2)
+
+a3 = 10
+b3 = 20
+aux = a3
+a3 = b3
+b3 = aux
+print(a3, b3)
