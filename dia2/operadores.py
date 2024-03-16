4d5ca472656a85a020db32350e780773aff22ef1
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-15 22:37:56 -0300

    Create operadores.py

---
+++ dia2/operadores.py
@@ -0,0 +1,9 @@
+a = int(input("Defina um valor inteiro para A: "))
+b = int(input("Defina um valor inteiro para B: "))
+
+if a > b:
+    print("A é maior que B")
+elif b > a:
+    print("B é maior que A")
+else:
+    print("As letras possuem valor igual")
