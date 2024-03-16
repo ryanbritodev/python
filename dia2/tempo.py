9475d7d24bfff9701ce6a6b9e0abb84f22d95eb0
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-15 22:27:38 -0300

    Update tempo.py

--- dia2/tempo.py
+++ dia2/tempo.py
@@ -1,9 +1,11 @@
-print("CONVERSÃO DE SEGUNDOS EM DIAS, HORAS, MINUTOS E SEGUNDOS")
+print("CONVERSÃO DE SEGUNDOS EM MESES, SEMANAS, DIAS, HORAS, MINUTOS E SEGUNDOS")
 
 tempo = int(input("Insira a quantidade de segundos: "))
+meses = int(tempo // 2.628e+6)
+semanas = tempo // 604800
 dias = tempo // 86400
 horas = tempo // 3600
 minutos = (tempo % 3600) // 60
 segundos = tempo % 60
 
-print(f"Essa quantidade de segundos representa {dias} dia(s), {horas} hora(s), {minutos} minuto(s) e {segundos} segundo(s).")
+print(f"Essa quantidade de segundos representa {meses} mês(es), {semanas} semana(s), {dias} dia(s), {horas} hora(s), {minutos} minuto(s) e {segundos} segundo(s).")
