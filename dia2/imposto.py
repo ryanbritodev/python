af411ec401c9a6647a3968ee0fb7ed0e475ef811
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-19 13:15:33 -0300

    Lição do Slide

---
+++ dia2/imposto.py
@@ -0,0 +1,6 @@
+# Expressão para determinar se uma pessoa deve ou não pagar imposto
+salario = int(input("Qual o seu salário? "))
+if salario > 1200:
+    print("Que pena! Você infelizmente deve pagar imposto.")
+else:
+    print("Parabéns! Você não precisa pagar imposto.")
--- dia2/nota.py
+++ dia2/nota.py
@@ -1,6 +1,8 @@
-n1 = 10
-n2 = 1
-n3 = 5
+# Exercício: Faça um programa que avalie a sua nota e lhe retorne verdadeiro ou falso sobre sua aprovação na disciplina
+
+n1 = int(input("Digite sua nota do primeiro trimestre: "))
+n2 = int(input("Digie sua nota do segundo trimestre: "))
+n3 = int(input("Digite sua nota do terceiro trimestre: "))
 
 nota = n1 + n2 + n3
 
