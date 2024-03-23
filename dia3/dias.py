48f70985036c02f93c61f3f58556ad5285443517
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-22 22:01:19 -0300

    Novas features

--- dia3/dias.py
+++ dia3/dias.py
@@ -2,7 +2,7 @@ print("CONVERSÃO DE DIAS, HORAS, MINUTOS E SEGUNDOS PARA SEGUNDOS")
 dias = int(input("Insira a quantidade de dias: "))
 horas = int(input("Insira a quantidade de horas: "))
 minutos = int(input("Insira a quantidade de minutos: "))
-segundos =  int(input("Insira a quantidada de segundos: "))
+segundos = int(input("Insira a quantidada de segundos: "))
 
 diaEmSegundo = dias * 86400
 horaEmSegundo = horas * 3600
