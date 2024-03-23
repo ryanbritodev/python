cd6f1c84546b0549c6d9ffad934617ae9e952e70
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-22 21:46:40 -0300

    Novos programas

---
+++ dia3/idade.py
@@ -0,0 +1,4 @@
+usuario = input("Qual o seu nome? ")
+anos = input("Qual a sua idade? ")
+
+print("Olá! Meu nome é", usuario, "e tenho", anos, "anos.")
---
+++ dia3/numeros.py
@@ -0,0 +1,10 @@
+# input sempre retorna como tipo STRING
+
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
+
+soma = (n1 + n2)
+
+print("A soma dos dois números é:", soma)
+
+# print(type(n1))
