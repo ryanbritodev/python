1ff2f0bbca5630b1af7a6b5b863cab9bf8748b52
Ryan Brito <ryanbritoramos@gmail.com>
2024-03-23 15:04:52 -0300

    Arrumando alguns códigos

--- dia3/casos.py
+++ dia3/casos.py
@@ -1,19 +1,22 @@
 print("CONDIÇÕES DE UM NÚMERO")
 
 numero = int(input("Digite o valor do número: "))
+resto = numero % 2
 
-if numero % 2 == 0:
+# IF ALINHADO - SÓ EXECUTA SE O PRIMEIRO CONDICIONAL FOR VERDADEIRO E O SEGUNDO TAMBÉM
+
+if resto == 0:
     if numero < 100:
         print("Seu número é par e menor que 100.")
 
-if numero % 2 == 0:
+if resto == 0:
     if numero >= 100:
         print("Seu número é par e maior ou igual a 100.")
 
-if numero % 1 == 0:
+if resto > 0:
     if numero < 100:
         print("Seu número é ímpar e menor que 100.")
 
-if numero % 1 == 0:
+if resto > 0:
     if numero >= 100:
-        print("Seu número é par e maior ou igual a 100.")
+        print("Seu número é ímpar e maior ou igual a 100.")
--- dia3/input.py
+++ dia3/input.py
@@ -1,2 +1,2 @@
-nome = input("Qual o seu nome?")
-print("Seu nome é", nome) + "."
+nome = input("Qual o seu nome? ")
+print("Seu nome é", nome)
