fa004d939d7347a8298172bd12555139c227c159
Ryan Brito <ryanbritoramos@gmail.com>
2024-04-12 22:48:39 -0300

    numeros

---
+++ dia4/repeticao3.py
@@ -0,0 +1,13 @@
+''' numero = 0
+while numero <= 100:
+    print(numero)
+    numero = numero + 2 '''
+
+numero = 1
+while numero < 101:
+    if numero%2 == 0:
+        print(f"{numero} é par")
+    else:
+        print(f"{numero} é ímpar")
+    numero = numero + 1
+    
