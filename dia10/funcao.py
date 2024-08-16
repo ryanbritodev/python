460a2c1752bd0d86a6a7ea8cd119b133ffc029ac
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-16 19:50:23 -0300

    Correçao Exercicios

--- dia10/funcao.py
+++ dia10/funcao.py
@@ -1,1 +1,19 @@
-yx
+valor = int(input("Vamos calcular uma função matemática?"))
+
+def y(x):
+    if x <= 2:
+        return x
+    elif x <= 3.5:
+        return 2
+    elif x < 5:
+        return 3
+    else:
+        return x**2 - 10 * x + 28
+
+# Uma função é definida da seguinte forma:
+# def nome(parâmetro1, parâmetro2, ..., parâmetro3):
+#   comandos
+#   return valor do retorno
+# Os parâmetros são variáveis, que são inicialização com valores
+# indicados durante a chamada/invocação da função
+# O comando return devolve para o invocador da função o resultado de execução desta
